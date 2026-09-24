21e08b946f2dac8e889e73e587c93f3f52e87357
Oleg Makarov <oemakarov@gmail.com>
2026-09-25 00:13:01 +0300

    Add transliteration helper for cyrillic slugs

---
+++ totum/common/helpers/SlugHelper.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace totum\common\helpers;
+
+class SlugHelper
+{
+    public static function transliterate(string $value): string
+    {
+        $map = [
+            'а' => 'a', 'б' => 'b', 'в' => 'v', 'г' => 'g', 'д' => 'd',
+            'е' => 'e', 'ё' => 'e', 'ж' => 'zh', 'з' => 'z', 'и' => 'i',
+            'й' => 'y', 'к' => 'k', 'л' => 'l', 'м' => 'm', 'н' => 'n',
+            'о' => 'o', 'п' => 'p', 'р' => 'r', 'с' => 's', 'т' => 't',
+            'у' => 'u', 'ф' => 'f', 'х' => 'h', 'ц' => 'c', 'ч' => 'ch',
+            'ш' => 'sh', 'щ' => 'sch', 'ъ' => '', 'ы' => 'y', 'ь' => '',
+            'э' => 'e', 'ю' => 'yu', 'я' => 'ya',
+        ];
+        return strtr(mb_strtolower($value), $map);
+    }
+
+    public static function make(string $value): string
+    {
+        return TextHelper::slugify(self::transliterate($value));
+    }
+}
